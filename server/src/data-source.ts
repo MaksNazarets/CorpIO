2ef55264dee397246f92296721a72e2254a9b76b
unknown <nazarets.maxim@gmail.com>
2025-03-13 20:47:34 +0200

    move db connection data to env variables

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -15,15 +15,11 @@ import { ProjectAttachment } from "./entity/ProjectAttachment";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  // host: "localhost",
-  // port: 5433,
-  // username: "postgres",
-  // password: process.env.DB_PASSWORD,
-  // database: "CorpIO_New",
-  host: "ep-sparkling-shadow-a26vy6bi-pooler.eu-central-1.aws.neon.tech",
-  username: "neondb_owner",
-  password: process.env.DB2_PASSWORD,
-  database: "neondb",
+
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 
   synchronize: true,
   logging: false,
